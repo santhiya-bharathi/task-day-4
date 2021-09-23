b28c514438149b39039f25bca96523e3bdc4863f
santhiya-bharathi <91201000+santhiya-bharathi@users.noreply.github.com>
2021-09-23 09:15:05 +0530

    Add files via upload

---
+++ indexday4.js
@@ -0,0 +1,64 @@
+//1.How to compare two JSON have the same properties without order?
+//a.	var obj1 = { name: "Person 1", age:5 };
+//b.	var obj2 = { age:5, name: "Person 1" };
+
+var obj1 = { name: "Person 1", age:5 };
+var obj2 = { age:5, name: "Person 1" };
+var result = true;
+
+if(Object.keys(obj1).length==Object.keys(obj2).length){
+    for(var key in obj1) { 
+        if(obj1[key] == obj2[key]) {
+            continue;
+        }
+        else {
+            result=false;
+            break;
+        }
+    }
+}
+else {
+    result=false;
+}
+console.log(result);
+
+
+//qn.no 2 answer this rest countries url v3 (in task qn doesn't work)
+const getBlogs = () => {
+    const xhr = new XMLHttpRequest();
+    xhr.open("GET", "https://restcountries.com/v3/all");
+    xhr.responseType = "json";
+    xhr.onload = () => {
+        console.log(xhr.response);
+        const blogs = xhr.response;
+        for(let blog of blogs){
+            //print capital only
+            console.log("capital:" + blog["capital"]);
+            
+        }
+    };
+        xhr.send();
+    
+};
+getBlogs();
+
+
+
+//qn.no 3 answer this rest countries url v3 (in task qn doesn't work)
+const getBlogs1 = () => {
+    const xhr = new XMLHttpRequest();
+    xhr.open("GET", "https://restcountries.com/v3/all");
+    xhr.responseType = "json";
+    xhr.onload = () => {
+        console.log(xhr.response);
+        const blogs1 = xhr.response;
+        for(let blog1 of blogs1){
+           
+            //print region, subregion,independent
+            console.log("region:" + blog1["region"],",subregion:" + blog1["subregion"],",independent:" + blog1["independent"]);
+        }
+    };
+        xhr.send();
+    
+};
+getBlogs1();
